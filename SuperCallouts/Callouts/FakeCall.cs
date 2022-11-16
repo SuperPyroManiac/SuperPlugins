299c14345a333d0218a7cf965b97fb67805561da
SuperPyroManiac <randellkahley@gmail.com>
2022-11-15 18:00:36 -0700

    Start work on #29

---
+++ SuperCallouts/Callouts/FakeCall.cs
@@ -0,0 +1,43 @@
+﻿using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using Rage;
+using SuperCallouts.SimpleFunctions;
+
+namespace SuperCallouts.Callouts;
+
+[CalloutInfo("DeadBody", CalloutProbability.Medium)]
+internal class FakeCall : Callout
+{
+    private Vector3 _spawnPoint;
+    private Blip _cBlip;
+    private float _heading;
+    private bool _onScene;
+
+    public override bool OnBeforeCalloutDisplayed()
+    {
+        CFunctions.FindSideOfRoad(750, 280, out _spawnPoint, out _heading);
+        ShowCalloutAreaBlipBeforeAccepting(_spawnPoint, 10f);
+        CalloutMessage = "~r~" + Settings.EmergencyNumber + " Report:~s~ Emergency call dropped.";
+        CalloutAdvisory = "Call dropped and dispatch is unable to reach caller back.";
+        CalloutPosition = _spawnPoint;
+        Functions.PlayScannerAudioUsingPosition(
+            "ATTENTION_ALL_UNITS_05 WE_HAVE CRIME_AMBULANCE_REQUESTED_01 IN_OR_ON_POSITION",
+            _spawnPoint);//TODO: Change this audio
+        return base.OnBeforeCalloutDisplayed();
+    }
+
+    public override bool OnCalloutAccepted()
+    {
+        return base.OnCalloutAccepted();
+    }
+
+    public override void Process()
+    {
+        base.Process();
+    }
+    
+    public override void End()
+    {
+        base.End();
+    }
+}
