1662f5f915e09345714d7c73f6f43f6e033a9add
SuperPyroManiac <randellkahley@gmail.com>
2023-01-01 10:54:04 -0700

    Revert

---
+++ SuperCallouts/Main.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+using LSPD_First_Response.Mod.API;
+using Rage;
+
+namespace SuperCallouts
+{
+    internal class Main : Plugin
+    {
+        internal static bool UsingUb { get; set; }
+        internal static bool UsingCi { get; set; }
+        
+        public override void Initialize()
+        {
+            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+            Settings.LoadSettings();
+            Game.LogTrivial("SuperCallouts " +
+                            Assembly.GetExecutingAssembly().GetName().Version +
+                            " by SuperPyroManiac has been initialised.");
+            Game.LogTrivial("Go on duty with LSPDFR to fully load SuperCallouts.");
+            Game.AddConsoleCommands();
+        }
+        
+        private void OnOnDutyStateChangedHandler(bool onDuty)
+        {
+            if (onDuty)
+                GameFiber.StartNew(delegate
+                {
+                    GameFiber.Wait(10000);
+                    UsingUb = CFunctions.IsLoaded("UltimateBackup");
+                    if (UsingUb) Game.LogTrivial("SuperCallouts: Using UltimateBackup API.");
+                    UsingCi = CFunctions.IsLoaded("CalloutInterface");
+                    if (UsingCi) Game.LogTrivial("SuperCallouts: Using CalloutInterface API.");
+                    RegisterCallouts();
+                    VersionChecker.IsUpdateAvailable();
+                });
+        }
+        
+        public override void Finally()
+        {
+            Game.LogTrivial("SuperCallouts - WARNING: LSPDFR has been unloaded or player is off duty!");
+            Game.LogTrivial("SuperCallouts by SuperPyroManiac has been disabled.");
+        }
+        
+        private static void RegisterCallouts()
+        {
+            if (Settings.HotPursuit) { Functions.RegisterCallout(typeof(HotPursuit)); Game.LogTrivial("SuperCallouts: HotPursuit Enabled"); }
+            Game.DisplayNotification("3dtextures", "mpgroundlogo_cops", "~r~SuperCallouts", "~g~Plugin Loaded.",
+                "SuperCallouts version: " +
+                Assembly.GetExecutingAssembly().GetName().Version + " loaded.");
+        }
+    }
+}
---
+++ SuperCallouts/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+using Rage;
+
+namespace SuperCallouts
+{
+    internal static class Settings
+    {
+        internal static bool Animals = true;
+        internal static Keys Interact = Keys.Y;
+        internal static Keys EndCall = Keys.End;
+        internal static string EmergencyNumber = "911";
+        internal static readonly string CalloutVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        internal static void LoadSettings()
+        {
+            Game.LogTrivial("Loading SuperCallouts config.");
+            var path = "Plugins/LSPDFR/SuperCallouts.ini";
+            var ini = new InitializationFile(path);
+            ini.Create();
+            //SwatCallouts
+            //BasicCallouts
+            Animals = ini.ReadBoolean("Settings", "AttackingAnimal", true);
+            //Other
+            Interact = ini.ReadEnum("Keys", "Interact", Keys.Y);
+            EndCall = ini.ReadEnum("Keys", "EndCall", Keys.End);
+            EmergencyNumber = ini.ReadString("Msc", "EmergencyNumber", "911");
+            Game.LogTrivial("SuperCallouts: Config loaded.");
+        }
+    }
+}
