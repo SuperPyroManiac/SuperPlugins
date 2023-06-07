0b9fd70944f106099a96db2b3a4779e599b8d5b7
Vari <72755263+variapolis@users.noreply.github.com>
2023-06-08 00:59:19 +0100

    feat(console-commands): add console command to force events

--- SuperEvents/EventFunctions/ConsoleCommands.cs
+++ SuperEvents/EventFunctions/ConsoleCommands.cs
@@ -1,5 +1,8 @@
+using System;
 using Rage;
 using Rage.Attributes;
+using Rage.ConsoleCommands;
+using Rage.ConsoleCommands.AutoCompleters;
 
 namespace SuperEvents.EventFunctions
 {
@@ -18,13 +21,33 @@ namespace SuperEvents.EventFunctions
             Game.DisplayNotification("3dtextures", "mpgroundlogo_cops", "~r~SuperEvents", "~g~Plugin Status:",
                 "SuperEvents paused: " + Main.PluginPaused);
         }
-        
+
         [ConsoleCommand]
         public static void Command_SEListEvents()
         {
             Game.LogTrivial("SuperEvents: Listing all Events========");
-            Game.LogTrivial(string.Join(", ", Events.AllEvents));
+            Game.LogTrivial(string.Join(", ", EventManager.AllEvents));
             Game.LogTrivial("=======================================");
+        }
+
+        [ConsoleCommand]
+        public static void Command_SEForceEvent(
+            [ConsoleCommandParameter(AutoCompleterType = typeof(EventParameterAutoCompleter))] string eventName) =>
+            EventManager.ForceEvent(eventName);
+    }
+
+    [ConsoleCommandParameterAutoCompleter(typeof(string))]
+    public class EventParameterAutoCompleter : ConsoleCommandParameterAutoCompleter
+    {
+        public EventParameterAutoCompleter(Type type) : base(type)
+        {
+        }
+    
+        public override void UpdateOptions()
+        {
+            Options.Clear();
+            foreach (var eventType in EventManager.AllEvents)
+                Options.Add(new AutoCompleteOption(eventType.Name, eventType.Name, "An Event"));
         }
     }
 }
