4f76b540ab226145189356e9c2a4567582a6c7a6
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2023-07-10 11:25:31 -0600

    Update AssemblyInfo.cs

--- TurnOffThatEngine/Properties/AssemblyInfo.cs
+++ TurnOffThatEngine/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SuperPyroManiac")]
 [assembly: AssemblyProduct("TurnOffThatEngine")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("Chocolate Milk")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
