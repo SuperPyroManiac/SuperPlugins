676ddb335cb6b7f95b982defa73be872fc21dd8e
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-04-08 10:26:49 -0600

    PC: Depend checker

---
+++ PyroCommon/Dependency.cs
@@ -0,0 +1,8 @@
+﻿namespace PyroCommon;
+
+internal class Dependency
+{
+    internal string PluginName { get; set; }
+    internal string DependName { get; set; }
+    internal string DependVersion { get; set; }
+}
