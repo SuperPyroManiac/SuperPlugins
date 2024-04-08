6a03618843003be1fc23aeeba8e78627b955ac55
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-04-08 12:00:48 -0600

    Fix wrapper for depend checker SC

--- SuperCallouts/Main.cs
+++ SuperCallouts/Main.cs
@@ -12,9 +12,12 @@ namespace SuperCallouts;
 
 internal class Main : Plugin
 {
+    internal static bool DependsGood { get; set; }
+    
     public override void Initialize()
     {
-        if (!DependChecker.Start()) return;
+        DependChecker.Start();
+        if (!DependsGood) return;
         Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
         Settings.LoadSettings();
         Game.AddConsoleCommands();
@@ -49,51 +52,54 @@ internal class Main : Plugin
     private static void RegisterCallouts()
     {
         //Reg
-        if (Settings.HotPursuit) { Functions.RegisterCallout(typeof(HotPursuit)); Log.Info("HotPursuit Enabled"); }
-        if (Settings.Robbery) { Functions.RegisterCallout(typeof(Robbery)); Log.Info("Robbery Enabled"); }
-        if (Settings.CarAccident) { Functions.RegisterCallout(typeof(CarAccident)); Log.Info("CarAccidents Enabled"); }
-        if (Settings.CarAccident) { Functions.RegisterCallout(typeof(CarAccident2)); }
-        if (Settings.CarAccident) { Functions.RegisterCallout(typeof(CarAccident3)); }
-        if (Settings.Animals) { Functions.RegisterCallout(typeof(AngryAnimal)); Log.Info("AngryAnimals Enabled"); }
-        if (Settings.Kidnapping) { Functions.RegisterCallout(typeof(Kidnapping)); Log.Info("Kidnapping Enabled"); }
-        if (Settings.TruckCrash) { Functions.RegisterCallout(typeof(TruckCrash)); Log.Info("TruckCrash Enabled"); }
-        if (Settings.PrisonTransport) { Functions.RegisterCallout(typeof(PrisonTransport)); Log.Info("PrisonTransport Enabled"); }
-        if (Settings.HitRun) { Functions.RegisterCallout(typeof(HitRun)); Log.Info("Hit and Run Enabled"); }
-        if (Settings.StolenCopVehicle) { Functions.RegisterCallout(typeof(StolenCopVehicle)); Log.Info("Stolen police vehicle Enabled"); }
-        if (Settings.StolenDumptruck) { Functions.RegisterCallout(typeof(StolenDumptruck)); Log.Info("Stolen dump truck Enabled"); }
-        if (Settings.AmbulanceEscort) { Functions.RegisterCallout(typeof(AmbulanceEscort)); Log.Info("AmbulanceEscort Enabled"); }
-        if (Settings.Aliens) { Functions.RegisterCallout(typeof(Aliens)); Log.Info("Aliens Enabled"); }
-        if (Settings.OpenCarry) { Functions.RegisterCallout(typeof(OpenCarry)); Log.Info("Open Carry Enabled"); }
-        if (Settings.Fire) { Functions.RegisterCallout(typeof(Callouts.Fire)); Log.Info("Fire Enabled"); }
-        if (Settings.OfficerShootout) { Functions.RegisterCallout(typeof(OfficerShootout)); Log.Info("OfficerShootout Enabled"); }
-        if (Settings.WeirdCar) { Functions.RegisterCallout(typeof(WeirdCar)); Log.Info("Suspicious Enabled"); }
-        if (Settings.Manhunt) { Functions.RegisterCallout(typeof(Manhunt)); Log.Info("Manhunt Enabled"); }
-        if (Settings.Impersonator) { Functions.RegisterCallout(typeof(Impersonator)); Log.Info("Impersonator Enabled"); }
-        if (Settings.ToiletPaperBandit) { Functions.RegisterCallout(typeof(ToiletPaperBandit)); Log.Info("ToiletPaperBandit Enabled"); }
-        if (Settings.BlockingTraffic) { Functions.RegisterCallout(typeof(BlockingTraffic)); Log.Info("Blocking Traffic Enabled"); }
-        if (Settings.IllegalParking) { Functions.RegisterCallout(typeof(IllegalParking)); Log.Info("Illegal Parking Enabled"); }
-        if (Settings.KnifeAttack) { Functions.RegisterCallout(typeof(KnifeAttack)); Log.Info("Knife Attack Enabled"); }
-        if (Settings.DeadBody) { Functions.RegisterCallout(typeof(DeadBody)); Log.Info("Dead Body Enabled"); }
-        if (Settings.FakeCall) { Functions.RegisterCallout(typeof(FakeCall)); Log.Info("FakeCall Enabled"); }
-        if (Settings.Trespassing) { Functions.RegisterCallout(typeof(Trespassing)); Log.Info("Trespassing Enabled"); }
-        if (Settings.Vandalizing) {Functions.RegisterCallout(typeof(Vandalizing)); Log.Info("Vandalizing Enabled");}
-        if (Settings.InjuredCop) {Functions.RegisterCallout(typeof(InjuredCop)); Log.Info("InjuredCop Enabled");}
-        if (Settings.IndecentExposure) {Functions.RegisterCallout(typeof(IndecentExposure)); Log.Info("IndecentExposure Enabled");}
+        if (Settings.HotPursuit) Functions.RegisterCallout(typeof(HotPursuit));
+        if (Settings.Robbery) Functions.RegisterCallout(typeof(Robbery));
+        if (Settings.CarAccident) Functions.RegisterCallout(typeof(CarAccident));
+        if (Settings.CarAccident) Functions.RegisterCallout(typeof(CarAccident2));
+        if (Settings.CarAccident) Functions.RegisterCallout(typeof(CarAccident3));
+        if (Settings.Animals) Functions.RegisterCallout(typeof(AngryAnimal));
+        if (Settings.Kidnapping) Functions.RegisterCallout(typeof(Kidnapping));
+        if (Settings.TruckCrash) Functions.RegisterCallout(typeof(TruckCrash));
+        if (Settings.PrisonTransport) Functions.RegisterCallout(typeof(PrisonTransport));
+        if (Settings.HitRun) Functions.RegisterCallout(typeof(HitRun));
+        if (Settings.StolenCopVehicle) Functions.RegisterCallout(typeof(StolenCopVehicle));
+        if (Settings.StolenDumptruck) Functions.RegisterCallout(typeof(StolenDumptruck));
+        if (Settings.AmbulanceEscort) Functions.RegisterCallout(typeof(AmbulanceEscort));
+        if (Settings.Aliens) Functions.RegisterCallout(typeof(Aliens));
+        if (Settings.OpenCarry) Functions.RegisterCallout(typeof(OpenCarry));
+        if (Settings.Fire) Functions.RegisterCallout(typeof(Callouts.Fire));
+        if (Settings.OfficerShootout) Functions.RegisterCallout(typeof(OfficerShootout));
+        if (Settings.WeirdCar) Functions.RegisterCallout(typeof(WeirdCar));
+        if (Settings.Manhunt) Functions.RegisterCallout(typeof(Manhunt));
+        if (Settings.Impersonator) Functions.RegisterCallout(typeof(Impersonator));
+        if (Settings.ToiletPaperBandit) Functions.RegisterCallout(typeof(ToiletPaperBandit));
+        if (Settings.BlockingTraffic) Functions.RegisterCallout(typeof(BlockingTraffic));
+        if (Settings.IllegalParking) Functions.RegisterCallout(typeof(IllegalParking));
+        if (Settings.KnifeAttack) Functions.RegisterCallout(typeof(KnifeAttack));
+        if (Settings.DeadBody) Functions.RegisterCallout(typeof(DeadBody));
+        if (Settings.FakeCall) Functions.RegisterCallout(typeof(FakeCall));
+        if (Settings.Trespassing) Functions.RegisterCallout(typeof(Trespassing));
+        if (Settings.Vandalizing) Functions.RegisterCallout(typeof(Vandalizing));
+        if (Settings.InjuredCop) Functions.RegisterCallout(typeof(InjuredCop));
+        if (Settings.IndecentExposure) Functions.RegisterCallout(typeof(IndecentExposure));
         //Swat
-        if (Settings.PrisonBreak) { Functions.RegisterCallout(typeof(PrisonBreak)); Log.Info("PrisonBreak Enabled"); }
-        if (Settings.Mafia1) { Functions.RegisterCallout(typeof(Mafia1)); Log.Info("Mafia1 Enabled"); }
-        if (Settings.Mafia2) { Functions.RegisterCallout(typeof(Mafia2)); Log.Info("Mafia2 Enabled"); }
-        if (Settings.Mafia3) { Functions.RegisterCallout(typeof(Mafia3)); Log.Info("Mafia3 Enabled"); }
-        if (Settings.Mafia4) { Functions.RegisterCallout(typeof(Mafia4)); Log.Info("Mafia4 Enabled"); }
-        if (Settings.LostMc) { Functions.RegisterCallout(typeof(LostGang)); Log.Info("LostMC Enabled"); }
-        if (Settings.Lsgtf) { Functions.RegisterCallout(typeof(Lsgtf)); Log.Info("LSGTF Enabled"); }
-        Game.DisplayNotification("3dtextures", "mpgroundlogo_cops", "~r~SuperCallouts", "~g~Plugin Loaded.",
-            "SuperCallouts version: " +
-            Assembly.GetExecutingAssembly().GetName().Version + " loaded.");
+        if (Settings.PrisonBreak) Functions.RegisterCallout(typeof(PrisonBreak));
+        if (Settings.Mafia1) Functions.RegisterCallout(typeof(Mafia1));
+        if (Settings.Mafia2) Functions.RegisterCallout(typeof(Mafia2));
+        if (Settings.Mafia3) Functions.RegisterCallout(typeof(Mafia3));
+        if (Settings.Mafia4) Functions.RegisterCallout(typeof(Mafia4));
+        if (Settings.LostMc) Functions.RegisterCallout(typeof(LostGang));
+        if (Settings.Lsgtf) Functions.RegisterCallout(typeof(Lsgtf));
+        Game.DisplayNotification("3dtextures",
+            "mpgroundlogo_cops",
+            "~r~SuperCallouts",
+            "~g~Plugin Loaded.",
+            "SuperCallouts version: " + Assembly.GetExecutingAssembly().GetName().Version + " loaded.");
     }
 
     public override void Finally()
     {
+        if (!DependsGood) return;
         if (VersionChecker.UpdateThread.IsAlive)
         {
             Log.Warning("Version thread still running during shutdown! Aborting thread...");
--- SuperCallouts/SimpleFunctions/DependChecker.cs
+++ SuperCallouts/SimpleFunctions/DependChecker.cs
@@ -8,7 +8,7 @@ namespace SuperCallouts.SimpleFunctions;
 
 public class DependChecker
 {
-    internal static bool Start()
+    internal static void Start()
     {
         if (!File.Exists("PyroCommon.dll"))
         {
@@ -19,24 +19,30 @@ public class DependChecker
             Game.Console.Print("======================================================");
             Game.Console.Print("SuperCallouts: Error Report End");
             Game.DisplayNotification("new_editor", "warningtriangle", "~r~SuperCallouts", "~y~Not Loaded!", "Plugin is installed incorrectly! Please see the RagePluginHook.log! Visit https://dsc.gg/ulss for help!"); 
-            return false;
+            return;
         }
         //TODO: Next version remove this as everyone should have updated PyroCommon 1.4
         var dependVersion = new Version(FileVersionInfo.GetVersionInfo("PyroCommon.dll").FileVersion);
         if (dependVersion < new Version("1.4.0.0"))
         {
-            Log.Error("PyroCommon.dll is outdated! Please reinstall the plugin correctly!");
+            Game.Console.Print("Oops there was an error here. Please send this log to https://dsc.gg/ulss");
+            Game.Console.Print("SuperCallouts: Error Report Start");
+            Game.Console.Print("======================================================");
+            Game.Console.Print($"PyroCommon.dll is outdated! Please reinstall plugin correctly!");
+            Game.Console.Print("======================================================");
+            Game.Console.Print("SuperCallouts: Error Report End");
             Game.DisplayNotification("new_editor", "warningtriangle", "~r~SuperCallouts", "~y~Not Loaded!", "Plugin is installed incorrectly! Please see the RagePluginHook.log! Visit https://dsc.gg/ulss for help!");
-            return false;
+            return;
         }
-        return Check();
+
+        if (File.Exists("PyroCommon.dll")) Check();
     }
 
-    internal static bool Check()
+    internal static void Check()
     {
         DependManager.AddDepend("PyroCommon.dll", "1.4.0.0");
         DependManager.AddDepend("RageNativeUI.dll", "1.9.2.0");
         DependManager.AddDepend("CalloutInterfaceAPI.dll", "1.0.3.0");
-        return DependManager.CheckDepends();
+        if (DependManager.CheckDepends()) Main.DependsGood = true;
     }
 }
