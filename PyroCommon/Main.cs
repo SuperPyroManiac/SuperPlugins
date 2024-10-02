9cb39cbc587015272c7598a673344eb851f2b048
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-10-02 11:14:55 -0600

    Load settings on start

--- PyroCommon/Main.cs
+++ PyroCommon/Main.cs
@@ -24,6 +24,7 @@ public static class Main
 
     internal static void InitCommon(string plugName, string plugVersion)
     {
+        Settings.LoadSettings();
          if ( InstalledPyroPlugins.ContainsKey(plugName) ) InstalledPyroPlugins.Remove(plugName);
          InstalledPyroPlugins.Add(plugName, plugVersion);
         if (_init) return;
