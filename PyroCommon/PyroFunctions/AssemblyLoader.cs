babdf829ac54146333651d75d34522073ee5e4b9
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-10-05 23:16:08 -0600

    Cleanup AssemblyLoader.cs

--- PyroCommon/PyroFunctions/AssemblyLoader.cs
+++ PyroCommon/PyroFunctions/AssemblyLoader.cs
@@ -10,27 +10,33 @@ public static class AssemblyLoader
 
     static AssemblyLoader()
     {
-        AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+        AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+    }
+
+    private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+    {
+        var assemblyName = new AssemblyName(args.Name).Name;
+        var resourceName = assemblyName switch
         {
-            var assemblyName = new AssemblyName(args.Name).Name;
-            var resourceName = assemblyName switch
-            {
-                "YamlDotNet" => "PyroCommon.Libs.YamlDotNet.dll",
-                _ => null
-            };
-            if (resourceName == null) return null;
-            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
-            if (stream != null)
-            {
-                var assemblyData = new byte[stream.Length];
-                stream.Read(assemblyData, 0, assemblyData.Length);
-                var assembly = Assembly.Load(assemblyData);
-                LoadedAssemblies[assemblyName] = assembly; // Strong reference to prevent unloading
-                return assembly;
-            }
+            "YamlDotNet" => "PyroCommon.Libs.YamlDotNet.dll",
+            _ => null
+        };
+
+        if (resourceName == null) return null;
+
+        using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        if (stream is null)
+        {
             Log.Error($"Resource {resourceName} not found.");
             return null;
-        };
+        }
+
+        var assemblyData = new byte[stream.Length];
+        stream.Read(assemblyData, 0, assemblyData.Length);
+        var assembly = Assembly.Load(assemblyData);
+        LoadedAssemblies[assemblyName] = assembly; // Strong reference to prevent unloading
+        return assembly;
     }
-    public static void Load(){}
+
+    public static void Load() { }
 }
